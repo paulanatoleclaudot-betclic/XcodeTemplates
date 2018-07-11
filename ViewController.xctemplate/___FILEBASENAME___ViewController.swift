aa44ee9d936f380a6aaa7180620e6ff6c420e292
Maxime de Chalendar <m.de-chalendar@betclicgroup.com>
2018-07-11 13:55:50 +0200

    Added ViewController & ViewModel templates

---
+++ ViewController.xctemplate/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,45 @@
+//___FILEHEADER___
+
+import UIKit
+
+/// A view controller responsible for <# what does this do ? #>
+final class ___VARIABLE_productName:identifier___ViewController: UIViewController, StoryboardBased {
+
+    // MARK: - Factory
+    static func instantiate(viewModel: ___VARIABLE_productName:identifier___ViewModel, analyticsService: IBetclicAnalyticsService) -> ___VARIABLE_productName:identifier___ViewController {
+        let vc = ___VARIABLE_productName:identifier___ViewController.instantiate()
+        vc.viewModel = viewModel
+        vc.analyticsService = analyticsService
+        return vc
+    }
+
+    // MARK: - Properties
+    private var viewModel: ___VARIABLE_productName:identifier___ViewModel!
+    private var analyticsService: IBetclicAnalyticsService!
+
+    // MARK: - IBOutlets
+
+    // MARK: - IBActions
+
+    // MARK: - View Controller life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupStaticContent()
+        setupBindings()
+    }
+
+}
+
+// MARK: - Setup
+private extension ___FILEBASENAMEASIDENTIFIER___ {
+
+    func setupStaticContent() {
+
+    }
+
+    func setupBindings() {
+
+    }
+
+}
